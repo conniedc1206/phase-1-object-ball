0410b9a1b67916cb159d009391beeb5ee878847d
Connie Park <conniedc1206@gmail.com>
2022-05-19 14:32:06 -0700

    Started creating gameObject

---
+++ src/00-objectball.js
@@ -0,0 +1,31 @@
+const gameObject = {
+    home = {
+        teamName:'Brooklyn Nets'
+        colors:['Black', 'White']
+        players = {
+            'Alan Anderson' : {}
+            number:
+            shoe:
+            points:
+            rebounds:
+            assists:
+            steals:
+            blocks:
+            slamDunks:
+        }
+    }
+    away = {
+        teamName:
+        colors:
+        players = {
+            number:
+            shoe:
+            points:
+            rebounds:
+            assists:
+            steals:
+            blocks:
+            slamDunks:
+        }
+    }
+}
